b4a54c3d9c72f41518c92a977cabff43986315fe
daciple-matthew <daciplerosemateo@gmail.com>
2025-03-24 11:13:30 +0800

    added single array

---
+++ MATEO_IT13B_Activities/src/mateo_single_array.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midterm;
+
+/**
+ *
+ * @author User
+ */
+public class mateo_single_array {
+        public static void main(String[] args) {
+        
+        // SINGLE ARRAY
+        char letters[]= {'A','B','C','D','E','F','G'}; 
+        
+        for (char l = 0; l < letters.length; l++) {
+            System.out.print(letters[l] + " , ");
+          
+        }
+        // 
+    }
+}
+
